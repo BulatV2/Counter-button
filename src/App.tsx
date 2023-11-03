10200ddd3ae16952c33ace13ff4613ed394debbd
Nino Bulat <ninobulat@gmail.com>
2023-11-03 14:17:53 +0100

    Button and CSS change
    Added second button to multiply original count, added margin for buttons

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,19 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const multiplyByTwo = () => {
+    setCount(count * 2)
+  }
+
   return (
     <>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Count is {count}
+        </button>
+        <button onClick={multiplyByTwo}>
+          Multiply by 2
         </button>
       </div>
     </>
